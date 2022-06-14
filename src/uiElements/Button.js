ebc2e2d5b19ed6077063a0d2eb44d04606168481
aryan rawther <aryanedu5786@gmail.com>
2022-06-14 08:37:36 +0530

    ui changes

--- src/uiElements/Button.js
+++ src/uiElements/Button.js
@@ -4,7 +4,7 @@ const Button = (props) => {
       <button
         value={props.id}
         onClick={props.onClick}
-        className="py-2 px-4 border-2 rounded-lg border-gray-800 h-10 w-40 hover:text-white hover:bg-gray-800"
+        className="py-1 border-2 rounded-lg border-gray-800 h-10 max-w-40 w-16 hover:text-white hover:bg-gray-800"
       >
         {props.id}
       </button>
